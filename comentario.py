4b3ec534426d98ad05a471260a146c3ae67d0866
AXEL <richardrickyy456@gmail.com>
2025-09-17 07:36:27 -0600

    Create comentario.py

---
+++ comentario.py
@@ -0,0 +1,11 @@
+"Este programa imprime la variable. Tambien observamos la mutabilidad de python"
+entero = 10
+flotante = 3.14
+cadena = "axel"
+booleano = True
+
+print (entero)
+print (flotante)
+
+#axel
+"ITPA"
